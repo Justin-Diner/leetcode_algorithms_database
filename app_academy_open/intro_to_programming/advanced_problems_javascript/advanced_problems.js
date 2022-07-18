b5cd0e3148482593d08e33cf9ae2205e16c510d7
Justin <90215009+DinerJ@users.noreply.github.com>
2022-07-17 23:00:41 -0400

    more problems

--- app_academy_open/intro_to_programming/advanced_problems_javascript/advanced_problems.js
+++ app_academy_open/intro_to_programming/advanced_problems_javascript/advanced_problems.js
@@ -150,3 +150,89 @@ const greatestFactor = (num) => {
 
 console.log(greatest_factor_array([16, 7, 9, 14]));
 console.log(greatest_factor_array([30, 3, 24, 21, 10]));
+
+//Perfect Square
+const perfect_square = (num) => {
+	for (let i = 0; i < num; i++) {
+		if (i * i == num) {
+			return true
+		}
+	}
+	return false;
+} 
+
+console.log(perfect_square(5));
+console.log(perfect_square(25));
+
+//Triple Sequence
+const triple_sequence = (start, length) => {
+	let answer = [start];
+
+	for (let i = 0; answer.length < length; i++) {
+		answer.push(answer[i] * 3);
+	}
+	return answer;
+}
+
+console.log(triple_sequence(2, 4));
+console.log(triple_sequence(4, 5));
+
+//Summation Sequence
+const summation_sequence = (start, length) => {
+	let answer = [start]; 
+
+	for (let i = 0; answer.length < length; i++) {
+		answer.push(summation(answer[i]));
+	}
+	return answer;
+}
+
+const summation = (num) => {
+	let summ = 0;
+
+	for (let i = 0; i <= num; i++) {
+		summ = summ + i;
+	}
+	return summ;
+}
+
+console.log(summation_sequence(3, 4));
+console.log(summation_sequence(5, 3));
+
+//Fibonacci
+const fibonacci = (length) => {
+	let fib = [1, 1];
+	if (length == 0) {
+		return [];
+	} else if(length == 1) {
+		return [1];
+	}
+
+	for (let i = 1; i < length-1; i++) {
+		let next = fib[i] + fib[i-1];
+		fib.push(next);
+	}
+	return fib;
+}
+
+console.log(fibonacci(0));
+console.log(fibonacci(1));
+console.log(fibonacci(6));
+console.log(fibonacci(8));
+
+//Caesar Cipher
+const caesar_cipher = (str, num) => {
+	let answer = "";
+	let alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+	for (let letter of str) {
+		let firstIndex = alphabet.indexOf(letter);
+		let nextIndex = alphabet.indexOf(letter) + num;
+		let nextChar = alphabet[nextIndex % 26];
+		console.log(nextChar);
+		answer + alphabet[nextChar];
+	}
+	return answer;
+}
+
+console.log(caesar_cipher("apple", 1));
